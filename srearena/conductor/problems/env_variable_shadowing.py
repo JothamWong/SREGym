7dde02bc174eab02bb5f2dcb3a1d36f8aa8b5305
Saad Mohammad Rafid Pial <saadmrp222@gmail.com>
2025-07-07 05:05:54 +0600

    format the code

--- srearena/conductor/problems/env_variable_shadowing.py
+++ srearena/conductor/problems/env_variable_shadowing.py
@@ -20,29 +20,27 @@ class EnvVariableShadowing(Problem):
             self.app = AstronomyShop()
         else:
             raise ValueError(f"Unsupported application: {self.app_name}")
-        
+
         super().__init__(app=self.app, namespace=self.app.namespace)
-        
+
         self.kubectl = KubeCtl()
         self.localization_oracle = LocalizationOracle(problem=self, expected=[self.faulty_service])
-        
+
         self.app.create_workload()
         self.mitigation_oracle = CompoundedOracle(
-            self,
-            MitigationOracle(problem=self),
-            WorkloadOracle(problem=self, wrk_manager=self.app.wrk)
+            self, MitigationOracle(problem=self), WorkloadOracle(problem=self, wrk_manager=self.app.wrk)
         )
-                
+
     @mark_fault_injected
     def inject_fault(self):
         print("== Fault Injection ==")
         injector = VirtualizationFaultInjector(namespace=self.namespace)
         injector.inject_env_variable_shadowing(microservices=[self.faulty_service])
         print(f"Service: {self.faulty_service} | Namespace: {self.namespace}")
-        
+
     @mark_fault_injected
     def recover_fault(self):
         print("== Fault Recovery ==")
         injector = VirtualizationFaultInjector(namespace=self.namespace)
         injector.recover_env_variable_shadowing(microservices=[self.faulty_service])
-        print(f"Service: {self.faulty_service} | Namespace: {self.namespace}") 
+        print(f"Service: {self.faulty_service} | Namespace: {self.namespace}")
